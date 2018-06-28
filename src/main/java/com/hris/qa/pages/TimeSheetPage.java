a0d04df02319f8390fe625c3ba6a81a025d6953d
vibhavsingh <40350892+vibhavsingh@users.noreply.github.com>
2018-06-28 11:57:48 +0530

    @nd change to TineSheetPage

--- src/main/java/com/hris/qa/pages/TimeSheetPage.java
+++ src/main/java/com/hris/qa/pages/TimeSheetPage.java
@@ -1,5 +1,34 @@
 package com.hris.qa.pages;
 
-public class TimeSheetPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.hris.qa.base.TestBase;
+
+public class TimeSheetPage extends TestBase{
+
+
+	@FindBy(css="head > title")
+	WebElement TimeSheetTitle;
+	
+	@FindBy(css="#drawerDiv > user-profile-sidebar > div > div.pro-dtl-brief > div > ul.list-unstyled.pro-details-left.marginTop15 > li:nth-child(2) > span.pro-dtl-val.ng-scope > a")
+    WebElement EmployeeName;
+	
+	public TimeSheetPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+	public String validateTimeSheetTitle()
+	{
+		return driver.getTitle();
+	}
+
+public boolean validateEmployeeName()
+{
+	return EmployeeName.isDisplayed();
+		
+	
+}
 }
